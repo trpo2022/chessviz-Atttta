c72d7515706ba68fc12c74b92989fe42cb772d19
Aylana Bichik <Atttta@users.noreply.github.com>
2022-05-03 20:05:21 +0700

    Added move function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define Height 8
 #define Widths 8
@@ -58,9 +59,10 @@ void Start_position(char a[Height][Widths])
     }
 }
 
-void Print_board(char a[Height][Widths])
+void Print_board(char a[Height][Widths], int hod)
 {
     int num = 1;
+    if(hod!=0) printf(" %d. \n",hod);
     for (int y = 0; y < Height; y++) {
         printf("%d", num);
         for (int x = 0; x < Widths; x++) {
@@ -72,18 +74,28 @@ void Print_board(char a[Height][Widths])
     printf("  a b c d e f g h\n");
 }
 
-void write_read(int *x, int*y, int *xx, int* yy, int* ex){
+void write_read(int *x, int*y, int *xx, int* yy, int* ex, int* mode){
 	int second=0;
 	char input[20];
+	*mode =0;
 	fflush(stdin);
-	printf("\n Write (# for breake) - ");
+	printf("\n Write - ");
 	//scanf("%c",input);
 	fgets(input,20,stdin);
 	input[strlen(input)-1]='\0';
 	for(int symbol=0;symbol<strlen(input);symbol++){
-		//printf("%c ",input[symbol]);
-		if (input[symbol] == '-')
-            second = 1;
+		//printf("%d ",symbol);
+		if(strlen(input)<5 && input[0]!='#'){
+			printf(" Error write. \n");
+			break;
+		}
+		else if(input[0]=='#'){
+			*ex+=2;
+		}
+		if (input[symbol] == '-'){
+            second = 1; *mode = 1;}
+        if (input[symbol] == 'x'){
+			second = 1; *mode = 2;}
         if (input[symbol] == '#')
             *ex += 2;
         if (input[symbol] == 'a' && second == 0)
@@ -159,23 +171,42 @@ void write_read(int *x, int*y, int *xx, int* yy, int* ex){
                 input[symbol] == '8' && second == 1) {
             *yy = 7;
         }
-	} 
-	
-	
-	
-	
+	}
+}
+
+void move_fig(char board[8][8], int* mode, int x, int y, int xx, int yy){
+	char obmen;
+	switch(*mode){
+		case 1:
+			if(board[yy][xx]!=' ') printf(" error, quite move. \n");
+			else{
+				board[yy][xx]=board[y][x];
+				board[y][x]=' ';
+			}
+		break;
+		case 2:
+			if(board[yy][xx]==' ') printf(" error, capturing. \n");
+			else{
+				board[yy][xx]=board[y][x];
+				board[y][x]=' ';
+			}
+		break;
+	}
 }
 
 int main()
 {
+	int hod=1, step=0;
     char board[Height][Widths];
     Start_position(board);
-    Print_board(board);
-    int x=0, y=0, x1=0, y1=0, end=0;
+    Print_board(board,0);
+    int x=0, y=0, x1=0, y1=0, end=0, mode=0;
     while(end==0){
-    	write_read(&x,&y,&x1,&y1,&end);
-    	printf(" %d-%d  %d-%d",x,y,x1,y1);
-    	//end++;
+    	write_read(&x,&y,&x1,&y1,&end,&mode);
+    	move_fig(board,&mode,x,y,x1,y1);
+    	Print_board(board,hod);
+    	step+=1;
+    	if(step==2){ hod+=1; step=0; }
 	}
     return 0;
 }
