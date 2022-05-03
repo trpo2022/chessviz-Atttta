bd938c6c3decd568f776211a720e4723e2bbf5dd
Aylana Bichik <Atttta@users.noreply.github.com>
2022-05-03 20:27:01 +0700

    Added function for print board

---
+++ src/lib/print_board.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Height 8
+#define Widths 8
+
+void Print_board(char a[Height][Widths], int hod)
+{
+    int num = 1;
+    if(hod!=0) printf(" %d. \n",hod);
+    for (int y = 0; y < Height; y++) {
+        printf("%d", num);
+        for (int x = 0; x < Widths; x++) {
+            printf(" %c", a[y][x]);
+        }
+        num++;
+        printf("\n");
+    }
+    printf("  a b c d e f g h\n");
+}
